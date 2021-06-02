1cd8e5a06db17671967f95f683a33f5047afcade
Rueben <75012722+Cosmic-Chimp@users.noreply.github.com>
2021-06-02 17:21:43 +0200

    few changes

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+// create references to the modal...
+var modal = document.getElementById('myModal');
+// to all images -- note I'm using a class!
+var images = document.getElementsByClassName('myImages');
+// the image in the modal
+var modalImg = document.getElementById("img01");
+// and the caption in the modal
+var captionText = document.getElementById("caption");
+
+// Go through all of the images with our custom class
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  // and attach our click listener for this image.
+  img.onclick = function(evt) {
+    console.log(evt);
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+
+//     OLD zoom in feature    //
+/*
 $(".img-popup").on('click', function() {
   $src = $(this).attr('src');
   $(".overlay-dark").css('display', 'block');
@@ -13,3 +44,4 @@ $(".overlay-dark").on('click', function() {
       $('.img-overlay').css('transform', 'translate(-50%, 0) scale(0, 0)');
   }, 600);
 });
+*/
