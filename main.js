42ae984fe0f3b61f6ac5473e3da8d9de51c8ccbc
Rueben <75012722+Cosmic-Chimp@users.noreply.github.com>
2021-06-03 14:56:53 +0200

    organizedabit

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 // create references to the modal...
-var modal = document.getElementById('myModal');
+let modal = document.getElementById('myModal');
 // to all images -- note I'm using a class!
-var images = document.getElementsByClassName('myImages');
+let images = document.getElementsByClassName('myImages');
 // the image in the modal
-var modalImg = document.getElementById("img01");
+let modalImg = document.getElementById("img01");
 // and the caption in the modal
-var captionText = document.getElementById("caption");
+let captionText = document.getElementById("caption");
 
 // Go through all of the images with our custom class
-for (var i = 0; i < images.length; i++) {
-  var img = images[i];
+for (let i = 0; i < images.length; i++) {
+  let img = images[i];
   // and attach our click listener for this image.
   img.onclick = function(evt) {
     console.log(evt);
@@ -19,7 +19,7 @@ for (var i = 0; i < images.length; i++) {
   }
 }
 
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
 span.onclick = function() {
   modal.style.display = "none";
